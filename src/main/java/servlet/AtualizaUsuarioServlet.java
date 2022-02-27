724f2bc3c6a3137323a5c64b468468f1af7f1a94
Daniel Jr <danielmarcelo.junior@hotmail.com>
2022-02-27 03:01:27 -0300

    refactor: Altera processamento de atualização de usuario para Servlet.
    Signed-off-by: Daniel Jr <danielmarcelo.junior@hotmail.com>

---
+++ src/main/java/servlet/AtualizaUsuarioServlet.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import entity.Usuario;
+import entity.dao.UsuarioDAO;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebServlet(name = "AtualizaUsuarioServlet", value = "/AtualizaUsuarioServlet")
+public class AtualizaUsuarioServlet extends HttpServlet {
+    Logger logger = Logger.getLogger(AtualizaUsuarioServlet.class.getSimpleName());
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            String nome = request.getParameter("inputName");
+            String rua = request.getParameter("addressInput");
+            String cep = request.getParameter("inputZip");
+            String cidade = request.getParameter("cityInput");
+
+            int numero;
+            try {
+                numero = Integer.parseInt(request.getParameter("numberInput"));
+            } catch (Exception e) {
+                numero = 0;
+            }
+
+            HttpSession session = request.getSession();
+            Object usuarioValidado = session.getAttribute("usuarioValidado");
+
+            if (usuarioValidado != null) {
+                Usuario usuario = (Usuario) usuarioValidado;
+
+                usuario.setNome(nome);
+                usuario.setRua(rua);
+                usuario.setCep(cep);
+                usuario.setNumero(numero);
+                usuario.setCidade(cidade);
+
+                if (!UsuarioDAO.atualizarUsuario(usuario)) {
+                    throw new Exception("Falha ao atualizar usuario.");
+                }
+
+                var message = String.format("Usuario %s atualizado com sucesso!", usuario.getNome());
+                logger.info(message);
+                response.sendRedirect("minha-conta.jsp?success=" + message);
+            }
+        } catch (Exception e) {
+            logger.warning(e.getMessage());
+            response.sendRedirect(request.getHeader("Referer") + "?error=" + e.getMessage());
+        }
+
+    }
+}
